437c7d293e848cb7c7b9c70aba5d7a6709111b32
Westin Miller <equinoxscripts@gmail.com>
2017-08-30 11:19:13 -0700

    Make local development versions clearly local dev

--- Versioning/AssemblyVersion.cs
+++ Versioning/AssemblyVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.1.4818.229")]
-[assembly: AssemblyInformationalVersion("1.1.229.265-52-gabf26c9")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("v0.0.0-dev")]
