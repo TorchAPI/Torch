8f755a5cfca53e869c1b477cd65e33d8eae8e9f8
Jimmacle <mindstorms11@cox.net>
2019-01-31 11:45:11 -0800

    Update Initializer.cs

--- Torch.Server/Initializer.cs
+++ Torch.Server/Initializer.cs
@@ -31,7 +31,7 @@ namespace Torch.Server
 
         private const string RUNSCRIPT = @"force_install_dir ../
 login anonymous
-app_update 298740 -beta playtest -betapassword nt7WuDw9kdvC
+app_update 298740 -beta playtest -betapassword nt8WuDw9kdvE
 quit";
 
         private TorchConfig _config;
