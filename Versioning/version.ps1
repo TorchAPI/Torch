b555f46f569af660fafe467d223c0958988c7b0c
Brant Martin <rexxar-tc@users.noreply.github.com>
2019-03-03 15:17:00 -0500

    Try versioning again

--- Versioning/version.ps1
+++ Versioning/version.ps1
@@ -1,9 +1,9 @@
-$buildSalt = $Env:BRANCH_NAME
+$buildSalt = $Env:BUILD_NUMBER
 $gitVersion = git describe --tags
 $gitSimpleVersion = git describe --tags --abbrev=0
 $simpleVersionStandard = echo $gitSimpleVersion | Select-String -Pattern "([0-9]+)\.([0-9]+)\.([0-9]+)" | % {$_.Matches} | %{$_.Groups[1].Value+"."+$_.Groups[2].Value+"."+$_.Groups[3].Value}
 $dotNetVersion = "$simpleVersionStandard.$buildSalt"
-$infoVersion = "$gitVersion" -replace "([0-9]+)\.([0-9]+)\.([0-9]+)","$dotNetVersion"
+$infoVersion = "$gitSimpleVersion" -replace "([0-9]+)\.([0-9]+)\.([0-9]+)","$dotNetVersion" + "$Env.BRANCH_NAME"
 
 $fileContent = @"
 using System.Reflection;
