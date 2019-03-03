dc5e7e4cb06b1fc06258ac352b28126a3bde7c7c
Brant Martin <rexxar-tc@users.noreply.github.com>
2019-03-03 15:01:51 -0500

    Try new version scheme

--- Versioning/version.ps1
+++ Versioning/version.ps1
@@ -1,4 +1,4 @@
-$buildSalt = $Env:BUILD_NUMBER
+$buildSalt = $Env:BRANCH_NAME
 $gitVersion = git describe --tags
 $gitSimpleVersion = git describe --tags --abbrev=0
 $simpleVersionStandard = echo $gitSimpleVersion | Select-String -Pattern "([0-9]+)\.([0-9]+)\.([0-9]+)" | % {$_.Matches} | %{$_.Groups[1].Value+"."+$_.Groups[2].Value+"."+$_.Groups[3].Value}
