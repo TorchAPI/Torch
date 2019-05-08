91ceb0aa22d8641b104f73cb8d66de06972c2a57
Brant Martin <rexxar-tc@users.noreply.github.com>
2019-05-08 18:34:09 -0400

    Implement ScrollContainerProperty to allow plugins to disable the default scrollviewer wrapper on their controls

---
+++ Torch.Server/Views/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Torch.Server.Views
+{
+    public static class Extensions
+    {
+        public static readonly DependencyProperty ScrollContainerProperty = DependencyProperty.RegisterAttached("ScrollContainer", typeof(bool), typeof(Extensions), new PropertyMetadata(true));
+
+        public static bool GetScrollContainer(this UIElement ui)
+        {
+            return (bool)ui.GetValue(ScrollContainerProperty);
+        }
+
+        public static void SetScrollContainer(this UIElement ui, bool value)
+        {
+            ui.SetValue(ScrollContainerProperty, value);
+        }
+    }
+}
