6c227b4a6ef0da59c72caab717e93daf1a8cc1b6
Olatide F <tide1988@live.com>
2021-09-03 08:27:25 -0400

    merge Friendly

--- Versioning/AssemblyVersion.cs
+++ Versioning/AssemblyVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
 [assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyInformationalVersion("v6.6.6-ElDiablo")]
+[assembly: AssemblyInformationalVersion("v0.0.0-dev")]
