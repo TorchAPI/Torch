7f675bd40732b3954568597805256f10fb3e95ca
Casimir <52760019+Casimir255@users.noreply.github.com>
2023-01-13 16:34:15 -0600

    Add LinkedEntity Shape (#530)
    * Cleaned up mod communications, and added new message type
    
    * Removed debug logs
    
    * Fixed OBB
    
    * Changed back to real mod
    
    * Uncomment clear player cache
    
    * Added linked entity
    
    * Fixed modID replacement

--- Torch.Mod/Messages/ShowDebugShape.cs
+++ Torch.Mod/Messages/ShowDebugShape.cs
@@ -1,8 +1,12 @@
 ﻿
 using ProtoBuf;
+using Sandbox.Game.Entities;
+using Sandbox.Game.World;
 using Sandbox.ModAPI;
 using System.Collections.Generic;
 using VRage.Game;
+using VRage.Game.Entity;
+using VRage.Game.ModAPI.Ingame;
 using VRageMath;
 using Color = VRageMath.Color;
 
@@ -13,7 +17,7 @@ namespace Torch.Mod.Messages
     {
         //Since we can only clear all renders, we can keep track of each one sent such that we can remove one but keep the rest
         public static Dictionary<string, DrawDebug> AllDraws = new Dictionary<string, DrawDebug>();
-        
+
 
         //Used as an identifier if you only want to remove one draw group
         [ProtoMember(301)]
@@ -42,15 +46,15 @@ namespace Torch.Mod.Messages
         public override void ProcessClient()
         {
             //must have a unique name
-            if(string.IsNullOrEmpty(uniqueName)) 
+            if (string.IsNullOrEmpty(uniqueName))
                 return;
 
-            if(remove)
+            if (remove)
             {
                 clear(uniqueName);
                 return;
             }
-            else if(removeAll)
+            else if (removeAll)
             {
                 clearAll();
                 return;
@@ -58,7 +62,7 @@ namespace Torch.Mod.Messages
 
             //MyAPIGateway.Utilities.ShowMessage("Torch", $"Hit process client on debug draw!");
 
-            if(!AllDraws.ContainsKey(uniqueName))
+            if (!AllDraws.ContainsKey(uniqueName))
                 AllDraws.Add(uniqueName, this);
 
             //If both are not false, then we have new items to draw
@@ -79,10 +83,23 @@ namespace Torch.Mod.Messages
             obj.position = position;
             obj.forward = forward;
             obj.up = up;
-            obj.color= color;
+            obj.color = color;
             obj.intensity = intensity;
             obj.linethickness = linethickness;
-            obj.raster= raster;
+            obj.raster = raster;
+
+            drawObjects.Add(obj);
+        }
+
+        public void addOBBLinkedEntity(long entityID, Color color, MySimpleObjectRasterizer raster, float intensity, float linethickness)
+        {
+            drawObject obj = new drawObject(drawObject.drawtype.OBBEntity);
+
+            obj.entityID = entityID;
+            obj.color = color;
+            obj.intensity = intensity;
+            obj.linethickness = linethickness;
+            obj.raster = raster;
 
             drawObjects.Add(obj);
         }
@@ -93,19 +110,21 @@ namespace Torch.Mod.Messages
 
             obj.position = position;
             obj.radius = radius;
-            obj.color= color;
+            obj.color = color;
             obj.intensity = intensity;
             obj.linethickness = linethickness;
-            obj.raster= raster;
+            obj.raster = raster;
 
             drawObjects.Add(obj);
         }
 
+        
+
 
         public static void clear(string uniquename)
         {
-            if(AllDraws.ContainsKey(uniquename))
-               AllDraws.Remove(uniquename);
+            if (AllDraws.ContainsKey(uniquename))
+                AllDraws.Remove(uniquename);
         }
 
         public static void clearAll()
@@ -116,7 +135,7 @@ namespace Torch.Mod.Messages
         //Updates individual method
         public void refreshDraw()
         {
-            foreach(var draw in drawObjects)
+            foreach (var draw in drawObjects)
             {
                 draw.update();
             }
@@ -136,14 +155,17 @@ namespace Torch.Mod.Messages
 
     }
 
-    
+
     [ProtoContract]
     public class drawObject
     {
-   
+
+        public MyEntity entRef;
+        public int searchAttempts = 0;
+
         public drawObject() { }
 
-        public drawObject(drawtype type) { this.type= type; }
+        public drawObject(drawtype type) { this.type = type; }
 
 
 
@@ -178,6 +200,9 @@ namespace Torch.Mod.Messages
         [ProtoMember(122)]
         public float linethickness = -1;
 
+        [ProtoMember(123)]
+        public long entityID = 0;
+
         //[ProtoMember(100)]
         //public MyOrientedBoundingBoxD obb;
 
@@ -185,12 +210,13 @@ namespace Torch.Mod.Messages
         public enum drawtype
         {
             OBB,
-            Sphere
+            Sphere,
+            OBBEntity,
         }
 
         public void update()
         {
-            
+
             switch (type)
             {
                 case drawtype.OBB:
@@ -198,6 +224,9 @@ namespace Torch.Mod.Messages
                     break;
                 case drawtype.Sphere:
                     drawSphere();
+                    break;
+                case drawtype.OBBEntity:
+                    drawOBBEntity();
                     break;
             }
         }
@@ -217,11 +246,32 @@ namespace Torch.Mod.Messages
             MySimpleObjectDraw.DrawTransparentSphere(ref transform, 10, ref color, raster, 25, material, material, -1);
         }
 
+        public void drawOBBEntity()
+        {
+            //This will keep updating the draw for live grid box preview
+            //Do not keep searching for entity on draw
+            if (searchAttempts > 10)
+                return;
+
+            if (entRef == null && entityID != 0)
+            {
+                entRef = MyEntities.GetEntityById(entityID);
+                searchAttempts++;
+            }
+
+
+            var material = TorchModCore.id;
+            var Matrix = entRef.WorldMatrix;
+            BoundingBoxD myAabb = entRef.PositionComp.LocalAABB;
+
+            MySimpleObjectDraw.DrawTransparentBox(ref Matrix, ref myAabb, ref color, raster, 1, linethickness, material, material);
+        }
+
 
 
 
 
 
     }
-    
+
 }
