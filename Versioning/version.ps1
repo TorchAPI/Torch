b2bf5958c6d1dab2274043f4ca4dc316c4bf56c0
Bishbash777 <50243964+Bishbash777@users.noreply.github.com>
2023-12-22 15:49:59 +0100

    Update version.ps1 (#573)

--- Versioning/version.ps1
+++ Versioning/version.ps1
@@ -1,5 +1,10 @@
-$buildSalt = $Env:BUILD_NUMBER
-$branchName = $Env:BRANCH_NAME
+$buildSalt = $ENV:BUILD_NUMBER
+$branchName = $ENV:BRANCH_NAME
+
+# Writing build salt and branch name
+Write-Host "Build Salt: $buildSalt"
+Write-Host "Branch Name: $branchName"
+
 $gitSimpleVersion = git describe --tags --abbrev=0
 $simpleVersionStandard = echo $gitSimpleVersion | Select-String -Pattern "([0-9]+)\.([0-9]+)\.([0-9]+)" | % {$_.Matches} | %{$_.Groups[1].Value+"."+$_.Groups[2].Value+"."+$_.Groups[3].Value}
 $dotNetVersion = "$simpleVersionStandard.$buildSalt"
@@ -14,4 +19,4 @@ using System.Reflection;
 
 echo $fileContent | Set-Content "$PSScriptRoot/AssemblyVersion.cs"
 
-echo "$infoVersion"
+echo "Information Version: $infoVersion"
