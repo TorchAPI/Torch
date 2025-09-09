26e2dc7361475a09b0d44ab691c225f9b77e4c36
Remaarn <mmusu3@live.com.au>
2025-09-09 16:28:30 +1000

    Improve MiscExtensions.ReadToEnd to fix exception when trying to get Length of a compressed stream.

--- Torch/Utils/MiscExtensions.cs
+++ Torch/Utils/MiscExtensions.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Threading;
 using Sandbox.Engine.Multiplayer;
 using Sandbox.Game.Entities;
 using Sandbox.Game.World;
@@ -12,52 +10,56 @@ namespace Torch.Utils
 {
     public static class MiscExtensions
     {
-        private static readonly ThreadLocal<WeakReference<byte[]>> _streamBuffer = new ThreadLocal<WeakReference<byte[]>>(() => new WeakReference<byte[]>(null));
-
-        private static long LengthSafe(this Stream stream)
+        public static byte[] ReadToEnd(this Stream stream, int optionalDataLength = -1)
         {
+            long streamLength = optionalDataLength;
+
             try
             {
-                return stream.Length;
+                if (stream.CanSeek)
+                    streamLength = stream.Length;
             }
             catch
             {
-                return 512;
             }
-        }
 
-        public static byte[] ReadToEnd(this Stream stream, int optionalDataLength = -1)
-        {
-            byte[] buffer;
-            if (!_streamBuffer.Value.TryGetTarget(out buffer))
-                buffer = new byte[stream.LengthSafe()];
-            var initialBufferSize = optionalDataLength > 0 ? optionalDataLength : stream.LengthSafe();
-            if (buffer.Length < initialBufferSize)
-                buffer = new byte[initialBufferSize];
-            if (buffer.Length < 1024)
-                buffer = new byte[1024];
-            var streamPosition = 0;
-            while (true)
+            int bufferSize = streamLength > 0 ? (int)streamLength : 32768;
+            var buffer = new byte[bufferSize];
+            int totalRead = 0;
+            int numRead;
+
+            do
             {
-                if (buffer.Length == streamPosition)
-                    Array.Resize(ref buffer, Math.Max((int)stream.LengthSafe(), buffer.Length * 2));
-                int count = stream.Read(buffer, streamPosition, buffer.Length - streamPosition);
-                if (count == 0)
-                    break;
+                int toRead = buffer.Length - totalRead;
 
-                streamPosition += count;
+                numRead = stream.Read(buffer, totalRead, toRead);
+
+                totalRead += numRead;
+
+                if (numRead > 0 && totalRead == buffer.Length)
+                {
+                    if (streamLength > 0)
+                        break;
+
+                    Array.Resize(ref buffer, buffer.Length * 2);
+                }
             }
+            while (numRead > 0);
 
-            var result = new byte[streamPosition];
+            var result = new byte[totalRead];
+
             Array.Copy(buffer, 0, result, 0, result.Length);
-            _streamBuffer.Value.SetTarget(buffer);
+
             return result;
         }
 
         public static IPAddress GetRemoteIP(this P2PSessionState_t state)
         {
-            // What is endianness anyway?
-            return new IPAddress(BitConverter.GetBytes(state.m_nRemoteIP).Reverse().ToArray());
+            // Reverse endianness
+            var bytes = BitConverter.GetBytes(state.m_nRemoteIP);
+            Array.Reverse(bytes);
+
+            return new IPAddress(bytes);
         }
 
         public static string GetGridOwnerName(this MyCubeGrid grid)
