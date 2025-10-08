ea252717c11869275182db3eed8b78c23119b0e9
Ryoichiro Oka <ryoka0418@gmail.com>
2025-10-08 12:31:43 +0900

    feat: log

--- Torch/Utils/DotEnv.cs
+++ Torch/Utils/DotEnv.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Text;
+using NLog;
 
 namespace Torch.Utils
 {
     public static class DotEnv
     {
+        static readonly ILogger Log = LogManager.GetCurrentClassLogger();
+
         public static void Load()
         {
             const string filePath = ".env";
@@ -14,21 +18,28 @@ namespace Torch.Utils
             var txt = File.ReadAllText(filePath, Encoding.Default);
             var newline = new[] { "\r\n", Environment.NewLine };
             var equal = new[] { "=" };
-            foreach (var cmp in txt.Split(newline, StringSplitOptions.RemoveEmptyEntries))
+            foreach (var line in txt.Split(newline, StringSplitOptions.RemoveEmptyEntries))
             {
-                var strArray = cmp.Split(equal, StringSplitOptions.None);
-                switch (strArray.Length)
+                var pair = line.Split(equal, StringSplitOptions.None);
+                if (pair.Length == 0) continue;
+
+                var key = pair[0].Trim();
+                if (pair.Length == 1)
                 {
-                    case 0:
-                        continue;
-                    case 1:
-                        Environment.SetEnvironmentVariable(strArray[0].Trim(), null);
-                        continue;
-                    default:
-                        Environment.SetEnvironmentVariable(strArray[0].Trim(), strArray[1].Trim());
-                        continue;
+                    Environment.SetEnvironmentVariable(key, null);
+                    Log.Info($"{key} (no value)");
+                    continue;
                 }
+
+                var value = pair[1].Trim();
+                Log.Info($"{key} = {Mask(value)} (masked)");
+                Environment.SetEnvironmentVariable(key, value);
             }
+        }
+
+        static string Mask(string value)
+        {
+            return string.Join("", Enumerable.Repeat('*', value.Length));
         }
     }
 }
